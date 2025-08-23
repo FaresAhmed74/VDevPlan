c3fa42a1c5e28206f99b7e0ca9eb119475acc0aa
FaresAhmed74 <faresahmed987@gmail.com>
2025-08-23 23:37:34 +0300

    s

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
     }
   }
 
-    backend "s3" {
-      bucket = "terraform--fareseldesouky2002--remoteserver"
-      key    = "eks-nginx/terraform.tfstate"
-      region = "us-east-1"
-      use_lockfile = true
-    }
+  backend "s3" {
+    bucket       = "terraform--fareseldesouky2002--remoteserver"
+    key          = "eks-nginx/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
