923f5fd70c3f01134c2d9eb885e818daf05616fb
FaresAhmed74 <faresahmed987@gmail.com>
2025-08-24 18:36:41 +0300

    fixed the backend s3 server

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,8 @@ terraform {
     bucket = "terraform--fareseldesouky2002--remoteserver"
     key = "fares/terraform/remote/s3/terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true
+    dynamodb_table = "terraform-locks"
+    
 
   }
 }
