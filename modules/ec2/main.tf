f302eb8bfc647380d4eb7a8eb8929569f90367a5
FaresAhmed74 <faresahmed987@gmail.com>
2025-08-24 18:44:14 +0300

    terraform validate

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -109,16 +109,12 @@ data "template_file" "userdata" {
     bucket_name  = var.s3_bucket_name
   }
 }
-resource "aws_key_pair" "eco_keypair" {
-  key_name   = "ecommerce-key"
-  public_key = file("~/.ssh/mykey.pub")
-}
+
 
 resource "aws_launch_template" "lt" {
   name_prefix   = "${var.project_name}-lt-"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.eco_keypair.key_name
 
   iam_instance_profile {
     name = var.instance_profile
